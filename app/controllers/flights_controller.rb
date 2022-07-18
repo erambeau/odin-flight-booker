82b7c2537a9ea46a848b76ebbf02427f3937dfff
erambeau <rambeaue@gmail.com>
2022-07-18 22:54:36 +0200

    First version of search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,10 @@
 class FlightsController < ApplicationController
 
     def index
+        @flights = Flight.all
+        @departure_hippoports = Hippoport.all.map{ |h| [ h.code, h.id ] }
+        @arrival_hippoports = @departure_hippoports
+        @date_dropdown  = Flight.all.map{ |f| [ f.datetime_depart, f.datetime_depart ] }
     end
 
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ApplicationRecord
   belongs_to :hippoport_depart , :class_name => "Hippoport"
   belongs_to :hippoport_arrival, :class_name => "Hippoport"
+
+
 end
