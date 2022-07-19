832c7e2d284a39c06f687affb0bcbe88671e3298
erambeau <rambeaue@gmail.com>
2022-07-19 17:45:11 +0200

    WIP nested attributes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,17 +4,30 @@ class BookingsController < ApplicationController
 
 
     def new
+        @booking = Booking.new
+
         @hippoports = Hippoport.all
         @selected_flight = Flight.find(params[:selected_flight])
         @nb_passengers = params[:nb_passengers].to_i
 
         #@wizards = Array.new(@nb_passengers) { Wizard.new }
-        wizard = Wizard.new
+        #@wizard = Wizard.new
 
     end
 
     def create
+        @booking = Booking.new(booking_params)
 
+        if @booking.save
+            redirect_to root_path
+        else
+            flash.now[:alert] = @booking.errors.full_messages
+        end  
     end
+
+    private
+	def booking_params
+		params.require(:booking).permit(:flight_id, :nbpassengers, :wizard, wizards: [:name, :email])
+	end
 
 end
